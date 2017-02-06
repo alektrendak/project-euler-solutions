960b908bf97025f66bcdffd3734ff111441a7264
Aleksander Trendak <alek.trendak@gmail.com>
2017-02-06 22:50:37 +0100

    Refactor in Problem 3 solution

--- problem003.py
+++ problem003.py
@@ -6,23 +6,21 @@
 #
 # What is the largest prime factor of the number 600851475143 ?
 
-import itertools
 import math
 
 N = 600851475143
 
 
 def fermat_factorization(n):
+    if n == 2:
+        return [n]
     a = math.ceil(math.sqrt(n))
     bb = a*a - n
     while not math.sqrt(bb).is_integer():
         a += 1
         bb = a*a - n
-    return list(itertools.chain.from_iterable(filter(
-        lambda x: x != [1],
-        [k for k in (fermat_factorization(i) if (i > 1 and i != n) else [i]
-                     for i in [a - math.sqrt(bb), a + math.sqrt(bb)])]
-    )))
+    return [k for results in (fermat_factorization(i) for i in [a - math.sqrt(bb), a + math.sqrt(bb)] if 1 < i < n)
+            for k in results] or [n]
 
 
 if __name__ == '__main__':
