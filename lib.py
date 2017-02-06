6b0e69ba8d870844a2739e895d55091091142c9a
Aleksander Trendak <alek.trendak@gmail.com>
2017-02-07 00:19:58 +0100

    Refactored fermat_factorization with use of gmpy2

--- lib.py
+++ lib.py
@@ -1,13 +1,20 @@
-import math
+import gmpy2
 
 
 def fermat_factorization(n):
-    if n == 2:
+    if n <= 3:
         return [n]
-    a = math.ceil(math.sqrt(n))
-    bb = a*a - n
-    while not math.sqrt(bb).is_integer():
+    if n % 2 == 0:
+        return [2] + fermat_factorization(n//2)
+    a = gmpy2.isqrt(n)
+    if a * a == n:
+        return [int(a), int(a)]
+    else:
         a += 1
-        bb = a*a - n
-    return [k for results in (fermat_factorization(i) for i in [a - math.sqrt(bb), a + math.sqrt(bb)] if 1 < i < n)
-            for k in results] or [n]
+    b2 = gmpy2.square(a) - n
+    while not gmpy2.is_square(b2):
+        a += 1
+        b2 = a*a - n
+    b = gmpy2.isqrt(b2)
+    return [int(k) for results in (fermat_factorization(i) for i in [a - b, a + b] if 1 < i < n)
+            for k in results] or [int(n)]
